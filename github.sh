9ab7c95f7fa73f162dfc005770eb5eda0c7c1c90
Aleksandra Janowska <dsw51712@student.dsw.edu.pl>
2024-05-09 17:08:34 +0200

    Komendy do tutoriala

---
+++ github.sh
@@ -0,0 +1,140 @@
+git commit
+git commit
+git branch bugFix
+git checkout bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+git checkout bugFix
+git checkout c3
+git checkout main
+git reset c1
+git reset c6
+git checkout bugFix
+git reset c0
+git checkout c1
+git reset c3
+git reset c1
+git checkout c2
+git revert c2
+git checkout pushed
+git reset c2'
+git revert c3
+git revert c4
+git revert c7
+git rebase -i c1
+git rebase -i c1
+git checkout main
+git reset c4'
+git rebase -i c1
+git rebase -i c3'
+git rebase -i c1
+git checkout main
+git reset c3''
+git checkout main
+git reset c3
+git rebase -i c1
+git reset c3
+git rebase -i c1
+git checkout c1
+git tag v0
+git checkout c2
+git tag v1
+git checkout bugFix
+git commit
+git checkout bugFix
+git rebase -i c2
+git checkout side
+git rebase -i c3'
+git checkout another
+git rebase -i c6'
+git checkout main
+git reset c7'
+git branch bugWork
+git checkout bugWork
+git reset c2
+git checkout main
+git checkout one
+git reset c4
+git rebase -i c1
+git checkout two
+git reset c5
+git rebase -i c1
+git checkout three
+git reset c2
+git clone
+git commit
+git reset c1
+git commit
+git reset c3
+git checkout c4
+git fetch
+git pull
+git commit
+git commit
+git clone
+git reset c1
+git commit 
+git pull
+git commit
+git commit
+git push
+git clone
+git commit
+git reset c1
+git commit
+git rebase c2
+git push
+git branch feature
+git reset c1
+git checkout feature
+git push
+git checkout main
+git pull
+git reset c2
+git rebase c8
+git reset c4
+git rebase c2'
+git reset c7
+git rebase c4'
+git push
+git checkout main
+git pull
+git merge c2
+git merge c4
+git merge c7
+git push
+git checkout -b side o/main
+git pull
+git reset c1
+git commit
+git rebase c2
+git push
+git push origin main
+git push origin foo
+git push origin c5:foo
+git push origin c4:main
+git fetch origin c6:main
+git fetch origin c3:foo
+git checkout foo
+git merge c6
+git fetch origin main:bar
+git push origin -d foo
+git fetch origin c3:foo
+git fetch origin c2:side
+git merge c3
+git merge c2
+
+
+
+
